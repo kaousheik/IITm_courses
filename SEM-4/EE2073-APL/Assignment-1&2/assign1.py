364158ae0201e2c36231eb464d05433d52a6c923
Ramanan <reachramzz25@gmail.com>
2020-01-20 23:27:22 +0530

    made some changes-2

---
+++ SEM-4/EE2073-APL/Assignment-1&2/assign1.py
@@ -0,0 +1,48 @@
+#Have to give argument in the terminal 
+
+def create_list(filename):
+    try:
+        f = open(filename)
+    except:
+        print("Incorrect File Name")
+        return [False]
+
+    data = f.read()
+    f.close()
+        
+    l = data.splitlines()
+    arr = []
+    data = []
+    if(('.circuit' in l) and ('.end' in l) ):
+        for i in l:
+            arr.append(i.split(' '))
+
+        arr_n = arr[::-1]
+        chk = True
+        start = False
+        i = 0
+        while(chk):
+            if(arr_n[i][0] == '.end'):
+                start = True
+                i += 1
+                continue
+            if(arr_n[i][0] == '.circuit'):
+                chk = False
+                break
+            if(start):
+                data.append([])
+                temp = list(reversed(arr_n[i]))
+                end = len(temp)
+                for l in range(len(temp)):
+                    if(arr_n[i][l]=='' or arr_n[i][l]==' '):
+                        end = l
+                        break
+                for j in range(len(temp)-end,len(temp)):
+                    data[-1].append(temp[j])
+                data[-1] = list(reversed(data[-1]))
+            i += 1
+        #Info from the file is extracted and stored in the list 'data'
+        return [True, data]
+    else:
+        print('Doesn\'t contain circuit info.' )
+        return [False]
---
+++ SEM-4/EE2073-APL/Assignment-1&2/assign2.py
@@ -0,0 +1,9 @@
+from assign1 import *
+import sys
+
+fname = sys.argv[1]
+l = create_list(fname)
+
+if(l[0]):
+    data = list(l[1])
+    #print(data)
